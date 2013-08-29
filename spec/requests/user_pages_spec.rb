daeb24ee5b26d241b943d992c5b3cef0598ab1c7
jesse <jesse@goodfellowsites.com>
2013-08-29 07:59:34 -0700

    ch. 8 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -53,7 +53,7 @@ end
 
         it { should have_title(user.name) }
         it { should have_link('Sign out') }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_welcome_message('Welcome') }
       end
 
       it "should create a user" do
